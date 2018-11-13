54a6b0351613c4f8d8f2ebb9cf1c3596ecb60c6d
MalcolmNSB <mbowles@math.ubc.ca>
2018-11-12 23:05:24 -0800

    update files

--- R/square.R
+++ R/square.R
@@ -1,11 +1,12 @@
-#' Apply a useful power
+#' Power and Box-Cox Transformations
 #'
-#' That's it -- these functions just square, cube, or take the
-#' reciprocal/inverse of a vector.
+#' Take the square, cube,
+#' reciprocal/inverse, or Box-Cox transform (with parameter lambda) of a vector.
 #'
-#' @param x The vector to be squared or cubed.
-#' @param plot_it Display a plot of \code{x} vs the output? Use logical.
-#' \code{FALSE} by default.
+#' @param x The vector of data.
+#' @param lambda A parameter for the Box-Cox transformation only, of length 1 or 2.
+#' @param plot_it Display a plot of \code{x} and a power of \code{x}? Use logical.
+#' \code{FALSE} by default. Not used for the Box-Cox transform.
 #'
 #' @return
 #' A vector that is the:
@@ -13,6 +14,10 @@
 #'      \item square (for \code{square})
 #'      \item cube (for \code{cube})
 #'      \item reciprocal/inverse (for \code{reciprocal})
+#'      \item Box-Cox transform, with either one or two parameters
+#'      (see https://en.wikipedia.org/wiki/Power_transform#Box%E2%80%93Cox_transformation for
+#'      further details on the transform.)
+#'
 #' }
 #'  of \code{x}.
 #'
@@ -23,6 +28,8 @@
 #' cube(tenvec)
 #' square(-5)
 #' reciprocal(2)
+#' Box_Cox(c(1,1.73,2.3), 0.5)
+#'
 #' @rdname square
 #' @export
 square <- function(x, plot_it=FALSE) pow(x, a=2, plot_it=plot_it)
@@ -36,19 +43,47 @@ cube <- function(x, plot_it=FALSE) pow(x, a=3, plot_it=plot_it)
 #' @export
 reciprocal <- function(x, plot_it=FALSE) pow(x, a=-1, plot_it=plot_it)
 
+
+
 #' @rdname square
 #' @export
-Box_Cox <- function(x, lambda, plot_it = FALSE){
-            if(all(x > 0)){
-                if(lambda != 0){
-                  (pow(x, a = lambda, plot_it=plot_it) - 1)/lambda
+Box_Cox_transform <- function(x, lambda){
+
+    if(length(lambda)> 2){
+        stop("lambda has to be of length 1 or 2")
+    }
+
+    if(length(lambda) == 1){
+        if(all(x > 0)){
+            if(lambda != 0){
+             (pow(x, a = lambda, FALSE) - 1)/lambda
+             }
+            else{
+             log(x)
+            }
+        }
+        else{
+            stop("vector input must have all positive entries")
+        }
+    }
+
+
+
+    else{
+        if(all(x > -lambda[2])){
+                if(lambda[1] != 0){
+                    (pow(x + lambda[2], a = lambda[1], FALSE) - 1)/lambda[1]
                 }
                 else{
-                   log(x)
+                 log(x + lambda[2])
                 }
-            }
-            else{
-                stop("vector input must have all positive entries")
-            }
-           }
+
+        }
+        else{
+            stop("entries of vector input must be greater than -lambda[2]")
+        }
+    }
+}
+
+
 
--- tests/testthat/test_square.R
+++ tests/testthat/test_square.R
@@ -12,3 +12,16 @@ test_that("Logicals automatically convert to numeric.", {
     expect_identical(square(logic_vec), logic_vec^2)
     expect_identical(cube(logic_vec), logic_vec^3)
 })
+
+test_that("Box_Cox returns logarithm of input when lambda = 0", {
+
+    expect_identical(Box_Cox_transform(c(1,2), 0), c(log(1), log(2)))
+
+})
+test_that("Different cases work", {
+    expect_identical(Box_Cox_transform(c(1,2), 1), c(0,1))
+    expect_identical(Box_Cox_transform(c(4,4), c(-1,-3)), c(0,0))
+    expect_identical(Box_Cox_transform(c(5,7), c(0,-3)), log(c(2,4)))
+    expect_identical(Box_Cox_transform(1,1), 0)
+
+})
